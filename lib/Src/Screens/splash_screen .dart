ac2ce593b8040b48fb5a4d6864aa7fb147c935f5
Mohamed <mohammedmohamuud.01@gmail.com>
2024-04-11 15:34:50 +0300

    Completed the splash screen and linked it to onboarding screen

---
+++ lib/Src/Screens/splash_screen .dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_to_do_list/Src/Screens/intro_screen.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(Duration(milliseconds: 3500), () {});
+    Get.offAll(
+      () => const IntroScreen(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 22, 22, 20),
+      body: SafeArea(
+        child: Center(
+          child: Image.asset("images/Logo.png"),
+        ),
+      ),
+    );
+  }
+}
