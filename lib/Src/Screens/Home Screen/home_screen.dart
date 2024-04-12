0c53dbbb5c46478acf18043e169a890bfae39d25
Mohamed <mohammedmohamuud.01@gmail.com>
2024-04-12 21:28:02 +0300

    Simple message

--- lib/Src/Screens/Home Screen/home_screen.dart
+++ lib/Src/Screens/Home Screen/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Hi"),
+        child: Text("Hi there"),
       ),
     );
   }
